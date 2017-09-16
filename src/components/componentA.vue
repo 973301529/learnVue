cc20113596fab85fe6c0a508b9c7c167d9b30183
Pinenut <973301529@qq.com>
2017-09-16 10:48:17 +0800

    My first vue Project

---
+++ src/components/componentA.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button v-on:click="onClickMe">open mouse!</button>
+    <h1>{{msgfromfather}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'hello from component a!'
+    }
+  },
+  props: ['msgfromfather'],
+  methods: {
+    onClickMe: function() {
+      console.log(this.msgfromfather)
+      this.$emit('child-tell-me-something',this.msg);
+          }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
